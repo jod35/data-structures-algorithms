797efe826e20bba3bfd8c0cf15982f7bd729c9d8
jon <jon@localhost.localdomain>
2019-12-20 12:12:37 +0300

    O(n) time complexity

---
+++ O(1).py
@@ -0,0 +1,7 @@
+#the steps to complete the algorithm remain constant
+def constant_algo(items):
+    result=items[0] * items[0]
+    print(result)
+
+constant_algo([4,5,6,8])
+
---
+++ On.py
@@ -0,0 +1,7 @@
+#this explains O(n) where the algorithms complexity grows linearly with amount of input given.
+
+def linear_algo(items):
+    for item in items:
+        print(item)
+
+linear_algo([1,2,3,4,5,6,7])
